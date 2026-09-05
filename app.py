543e9d19d1f337356dc94fdd9877daee72b132f8
bbsfood <bbsfood.id@gmail.com>
2026-09-05 11:19:15 +0700

    app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ DAFTAR_PRODUK = [
     "Seblak Mix", "Jengkol", "Kedelai Ori", "Kedelai Pedas", 
     "K. Tongkol Asin", "K. Tongkol Pedas", "Campuran (Mix)", 
     "Marneng Asin", "Marneng Pedas", "Emping Balado Asin", 
-    "Emping Pedas Manis", "Mie Enak", "K. Jablay", "Brondong", "Kedelai Mesin","Rambut Nenek"
+    "Emping Pedas Manis", "Mie Enak", "K. Jablay", "Brondong", "Kedelai Mesin","Rambut Nenek", "K.Manggar"
 ]
 UKURAN_BAL = ["Isi 10", "Isi 12"]
 
